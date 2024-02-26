2a089710a23dd8331e4e81f02cb6da5ac29edd00
Himanshu Pegu <imanshupegu777@gmail.com>
2024-02-26 23:10:01 +0530

    minor UI update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+const navbar = document.querySelector('.navbar')
+
+var lastScrollTop = 0
+window.addEventListener('scroll', () => {
+  var scrollTop = window.pageYOffset
+  if (scrollTop > lastScrollTop) {
+    navbar.style.transition = 'all .4s ease'
+    navbar.style.transition = 'translateY(-100%)'
+  } else {
+    navbar.style.transition = 'all .6s ease'
+    navbar.style.transition = 'translateY(0%)'
+  }
+  lastScrollTop = scrollTop
+})
+
 window.onload = function () {
   if (
     !sessionStorage.getItem('loaderDisplayed')
